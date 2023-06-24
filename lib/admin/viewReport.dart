8d17600b603fd78e8c3a70f37384deb9506e16f5
Joan <txuen006@gmail.com>
2023-06-25 07:39:22 +0800

    generate user rebate report

--- lib/admin/viewReport.dart
+++ lib/admin/viewReport.dart
@@ -1,4 +1,12 @@
+import 'dart:html' as html;
+import 'dart:typed_data';
+import 'package:intl/intl.dart';
+
+import 'package:dio/dio.dart';
+import 'package:dlsm_web/globalVar.dart' as globalVar;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class ReportPage extends StatefulWidget {
   const ReportPage({super.key});
@@ -8,8 +16,135 @@ class ReportPage extends StatefulWidget {
 }
 
 class _ReportPageState extends State<ReportPage> {
+  List userList = [];
+  List rebateRecords = [];
+  final pdf = pw.Document();
+  var anchor;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: const Text("Reort Page"));
+    return ListView.builder(
+        itemCount: userList == [] ? 0 : userList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+              child: ListTile(
+            title: Text(userList[index]['_id']),
+            subtitle: Text(
+                "${userList[index]['email']} ${userList[index]['phoneNumber']}"),
+            trailing: TextButton(
+              child: const Text("View Report"),
+              onPressed: () {
+                generateReport(userList[index]['_id']);
+              },
+            ),
+          ));
+        });
+  }
+
+  void getData() async {
+    try {
+      var response = await Dio()
+          .get('https://drive-less-save-more-1.herokuapp.com/admin/dashboard',
+              options: Options(headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+                "Authorization": "Bearer ${globalVar.token}",
+              }));
+      if (response.statusCode == 200) {
+        // print(response.data);
+        setState(() {
+          userList = response.data as List;
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  generateReport(uId) async {
+    await fetchRebate(uId);
+    final font = await rootBundle.load("assets/fonts/OpenSans-Regular.ttf");
+    final ttf = pw.Font.ttf(font);
+    pdf.addPage(pw.Page(
+      build: (pw.Context context) =>
+          pw.Table(border: pw.TableBorder.all(), children: [
+        pw.TableRow(children: [
+          pw.Container(height: 32, child: pw.Text('User ID')),
+          pw.Container(height: 32, child: pw.Text(uId))
+        ]),
+        pw.TableRow(
+          children: <pw.Widget>[
+            pw.Container(height: 32, child: pw.Text('Campaign')),
+            pw.Container(height: 32, child: pw.Text('Requested Date')),
+            pw.Container(height: 32, child: pw.Text('Rebate Type')),
+            pw.Container(height: 32, child: pw.Text("Status")),
+          ],
+        ),
+        ...rebateRecords
+            .map(
+              (e) => pw.TableRow(
+                children: <pw.Widget>[
+                  pw.Container(
+                      height: 32, child: pw.Text(e['campaign'].toString())),
+                  pw.Container(
+                      height: 32,
+                      child: pw.Text(DateFormat("yyyy-MM-dd h:mm:ss a")
+                          .format(DateTime.parse(e['requestedDate'])))),
+                  pw.Container(
+                      height: 32, child: pw.Text(e['rebateType'].toString())),
+                  pw.Container(height: 32, child: pw.Text(e['status'])),
+                ],
+              ),
+            )
+            .toList()
+      ]),
+    ));
+
+    await savePDF(uId);
+  }
+
+  savePDF(uId) async {
+    Uint8List pdfInBytes = await pdf.save();
+    final blob = html.Blob([pdfInBytes], 'application/pdf');
+    final url = html.Url.createObjectUrlFromBlob(blob);
+    anchor = html.document.createElement('a') as html.AnchorElement
+      ..href = url
+      ..style.display = 'none'
+      ..download = '$uId Rebate Report.pdf';
+    html.document.body!.children.add(anchor);
+
+    //to open the pdf
+    anchor!.click();
+  }
+
+  fetchRebate(uId) async {
+    try {
+      var response = await Dio()
+          .get('https://drive-less-save-more-1.herokuapp.com/view/records/$uId',
+              options: Options(headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+                "Authorization": "Bearer ${globalVar.token}",
+              }));
+      if (response.statusCode == 200) {
+        setState(() {
+          var data = response.data
+              .where((i) => i["user"] == uId)
+              .map((e) => e["rebateRecords"]);
+          data.forEach(
+              (nums) => nums.forEach((number) => rebateRecords.add(number)));
+          print(rebateRecords);
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 }
